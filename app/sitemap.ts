0bb03600f601eeeba7c56ee8598b8a0c85a85676
harshbharti55 <123297133+harshbharti55@users.noreply.github.com>
2026-07-06 16:37:02 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,34 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://kickboxer-harsh.online";
+
   return [
     {
-      url: 'https://kickboxer-harsh.online',
+      url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/articles/my-kickboxing-journey`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/articles/how-i-prepare-for-championships`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/articles/life-of-a-student-athlete`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/articles/nutrition-for-kickboxers`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/articles/world-cup-preparation`,
+      lastModified: new Date(),
     },
   ];
 }
