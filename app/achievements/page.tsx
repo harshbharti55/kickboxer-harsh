db1c8f6b1baecb3f19d47c1a6983d46fabf50d1c
harshbharti55 <123297133+harshbharti55@users.noreply.github.com>
2026-07-11 22:46:56 +0530

    Create page.tsx

---
+++ app/achievements/page.tsx
@@ -0,0 +1,9 @@
+import Achievements from "@/components/Achievements";
+
+export default function AchievementsPage() {
+  return (
+    <main className="pt-24">
+      <Achievements />
+    </main>
+  );
+}
