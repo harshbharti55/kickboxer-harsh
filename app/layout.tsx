a7688bc01b74e34a15e023086c5b528fb80869b6
harshbharti55 <123297133+harshbharti55@users.noreply.github.com>
2026-07-12 17:09:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -337,16 +337,20 @@ export default function RootLayout({
   strategy="afterInteractive"
 />
 
+<Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-4NWSL7S4S1"
+  strategy="afterInteractive"
+/>
+
 <Script id="google-analytics" strategy="afterInteractive">
   {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-4NW5L754S1');
+    gtag('config', 'G-4NWSL7S4S1');
   `}
 </Script>
-        
       </body>
     </html>
   );
