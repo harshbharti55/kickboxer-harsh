f43f1268ad3eae9568a461f77803df7632195e79
harshbharti55 <123297133+harshbharti55@users.noreply.github.com>
2026-07-17 23:35:01 +0530

    Update Particles.tsx

--- components/Particles.tsx
+++ components/Particles.tsx
@@ -1,1 +1,29 @@
+"use client";
 
+import { Points, PointMaterial } from "@react-three/drei";
+import { useMemo } from "react";
+import * as THREE from "three";
+
+export default function Particles() {
+  const particles = useMemo(() => {
+    const positions = new Float32Array(1500);
+
+    for (let i = 0; i < 1500; i++) {
+      positions[i] = (Math.random() - 0.5) * 15;
+    }
+
+    return positions;
+  }, []);
+
+  return (
+    <Points positions={particles} stride={3} frustumCulled={false}>
+      <PointMaterial
+        transparent
+        color="#FFD700"
+        size={0.05}
+        sizeAttenuation
+        depthWrite={false}
+      />
+    </Points>
+  );
+}
